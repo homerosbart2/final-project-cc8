63cb023329832fdb61c5965b254b4bfe34bcae3f
yingshiwu1997 <yingshi.wu@galileo.edu>
2019-09-27 17:08:33 -0600

    loading

--- Front-end/scriptBack.js
+++ Front-end/scriptBack.js
@@ -23,7 +23,7 @@ function platformContainer(nombre, ip, puerto, color, id) {
 	return `
 		<div class="plt-container">
 			<div class="col">
-				<div class="card shadow--sm" style="${color}" onclick="showPagePlt(${id})">
+				<div class="card shadow--sm loading" style="${color}" onclick="showPagePlt(${id})">
 					<div class="card--body">
 						<p class="card--text card--size-state">${nombre}</p>
 						<p class="card--text">${ip}:${puerto}</p>
