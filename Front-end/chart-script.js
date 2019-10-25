c295897c79fb109e55d8833843b9b581d7b8d329
yingshiwu1997 <yingshi.wu@galileo.edu>
2019-10-25 16:48:56 -0600

    estilos

--- Front-end/chart-script.js
+++ Front-end/chart-script.js
@@ -1,4 +1,6 @@
 var ctx = document.querySelector('#myChart');
+var ctx2 = document.querySelector('#myChart2');
+var ctx3 = document.querySelector('#myChart3');
 
 function createNewChart(labels, data){
     myChart = new Chart(ctx, {
@@ -35,13 +37,153 @@ function createNewChart(labels, data){
         }
     });
 }
+var myChart2 = new Chart(ctx2, { 
+    type: 'line',
+    data: {
+        labels: [...Array(9).keys()],
+        datasets: [{
+            label: 'Frecuencia',
+            data: [
+                {
+                    x: 0,
+                    y: 5
+                },
+                {
+                    x: 1,
+                    y: 26
+                },
+                {
+                    x: 2,
+                    y: 10
+                },
+                {
+                    x: 3,
+                    y: 2
+                },
+                {
+                    x: 4,
+                    y: 7
+                },
+                {
+                    x: 5,
+                    y: 5
+                },
+                {
+                    x: 6,
+                    y: 10
+                },
+                {
+                    x: 7,
+                    y: 10
+                },
+                {
+                    x: 8,
+                    y: 10
+                }
+            ],
+            responsive: true,
+            lineTension: 0.2,
+            pointStyle: 'circle',
+            pointRadius: 5,
+            backgroundColor: 'rgba(54, 162, 235, 0.3)',
+            pointBackgroundColor: 'rgba(54, 162, 235, 1)',
+            borderColor: '#ffdde1',
+            borderWidth: 3,
+            pointHoverBorderWidth: 8,
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
+var myChart3 = new Chart(ctx3, { 
+    type: 'bar',
+    data: {
+        labels: [...Array(9).keys()],
+        datasets: [{
+            label: 'Status',
+            data: [
+                {
+                    x: 0,
+                    y: 5
+                },
+                {
+                    x: 1,
+                    y: 26
+                },
+                {
+                    x: 2,
+                    y: 10
+                },
+                {
+                    x: 3,
+                    y: 2
+                },
+                {
+                    x: 4,
+                    y: 7
+                },
+                {
+                    x: 5,
+                    y: 5
+                },
+                {
+                    x: 6,
+                    y: 10
+                },
+                {
+                    x: 7,
+                    y: 10
+                },
+                {
+                    x: 8,
+                    y: 10
+                }
+            ],
+            responsive: true,
+            lineTension: 0.2,
+            pointStyle: 'circle',
+            pointRadius: 5,
+            backgroundColor: 'rgba(54, 162, 235, 0.3)',
+            pointBackgroundColor: 'rgba(54, 162, 235, 1)',
+            borderColor: '#ffdde1',
+            borderWidth: 3,
+            pointHoverBorderWidth: 8,
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 var myChart = new Chart(ctx, { 
     type: 'line',
     data: {
         labels: [...Array(9).keys()],
         datasets: [{
-            label: 'Datos',
+            label: 'Sensor',
             data: [
                 {
                     x: 0,
