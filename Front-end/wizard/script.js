2698e30da1d0e2815c2877e3babc42b071e14604
yingshiwu1997 <yingshi.wu@galileo.edu>
2019-10-26 21:17:11 -0600

    wizard

--- Front-end/wizard/script.js
+++ Front-end/wizard/script.js
@@ -1,11 +1,75 @@
 // Mi idea es que definamos funciones para cada pagina que 
 // necesitemos y que retornen la estructura HTML como string.
 const renderFirstPage = () => {
-    return `<div>Shinshé</div>`;
+    return `<div class = "plt-cont">
+                <input type="radio" name="plataforma" id="plt-1" value="plat" checked />
+                <label class= "plt-label" for="plt-1">Plataforma</label>
+                  
+                <input type="radio" name="plataforma" id="plt-2" value="plat" />
+                <label class= "plt-label" for="plt-2">Plataforma</label>
+                  
+                <input type="radio" name="plataforma" id="plt-3" value="plat" />
+                <label class= "plt-label" for="plt-3">Plataforma</label>
+
+                <input type="radio" name="plataforma" id="plt-4" value="plat" />
+                <label class= "plt-label" for="plt-4">Plataforma</label>
+
+                <input type="radio" name="plataforma" id="plt-5" value="plat" />
+                <label class= "plt-label" for="plt-5">Plataforma</label>
+
+                <input type="radio" name="plataforma" id="plt-6" value="plat" />
+                <label class= "plt-label" for="plt-6">Plataforma</label>
+            </div>`;
 }
 
 const renderSecondPage = () => {
-    return `<div>Wu</div>`;
+    return `<div class = "event-hardware">
+                <div class = "event-hardware-list">
+                    <input type="radio" name="hardware" id="hard-1" value="hard" checked />
+                    <label class= "plt-label" for="hard-1">Hardware</label>
+                      
+                    <input type="radio" name="hardware" id="hard-2" value="hard" />
+                    <label class= "plt-label" for="hard-2">Hardware</label>
+                </div>
+                <div class = "event-hardware-cmp">
+                    <input class = "freq-value" type = "number" placeholder = "Frecuencia" min = "0">
+                    <input class = "freq-value" type = "number" placeholder = "Valor del Sensor" min = "0">
+                    <input class = "freq-value" type = "text" placeholder = "Frecuencia">
+                </div>
+            </div>`;
+}
+
+const renderThirdPage = () => {
+    return `<div class = "event-hardware">
+                <div class = "event-hardware-list">
+                    <input type="radio" name="theb" id="then-1" value="hard" checked />
+                    <label class= "plt-label" for="then-1">Hardware</label>
+                      
+                    <input type="radio" name="then" id="then-2" value="hard" />
+                    <label class= "plt-label" for="then-2">Hardware</label>
+                </div>
+                <div class = "event-hardware-cmp">
+                    <input class = "freq-value" type = "number" placeholder = "Frecuencia" min = "0">
+                    <input class = "freq-value" type = "number" placeholder = "Valor del Sensor" min = "0">
+                    <input class = "freq-value" type = "text" placeholder = "Frecuencia">
+                </div>
+            </div>`;
+}
+const renderFourPage = () => {
+    return `<div class = "event-hardware">
+                <div class = "event-hardware-list">
+                    <input type="radio" name="else" id="else-1" value="hard" checked />
+                    <label class= "plt-label" for="else-1">Hardware</label>
+                      
+                    <input type="radio" name="else" id="else-2" value="hard" />
+                    <label class= "plt-label" for="else-2">Hardware</label>
+                </div>
+                <div class = "event-hardware-cmp">
+                    <input class = "freq-value" type = "number" placeholder = "Frecuencia" min = "0">
+                    <input class = "freq-value" type = "number" placeholder = "Valor del Sensor" min = "0">
+                    <input class = "freq-value" type = "text" placeholder = "Frecuencia">
+                </div>
+            </div>`;
 }
 
 // Deberíamos de definir las páginas así para que sea 
@@ -13,20 +77,20 @@ const renderSecondPage = () => {
 // páginas.
 const pages = [
     {
-        title: 'primera página',
+        title: 'Seleccionar Plt',
         renderer: renderFirstPage
     },
     {
-        title: 'segunda página',
+        title: 'Hardware',
         renderer: renderSecondPage
     },
     {
-        title: 'tercera página',
-        renderer: renderSecondPage
+        title: 'Then',
+        renderer: renderThirdPage
     },
     {
-        title: 'cuarta página',
-        renderer: renderFirstPage
+        title: 'Else',
+        renderer: renderFourPage
     }
 ]
 
